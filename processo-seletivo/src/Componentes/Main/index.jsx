799e6dccdeffe7a06cf04d0d82ad0c9f64365074
João Victor Matias de Oliveira <99759094+matiasJoao@users.noreply.github.com>
2022-05-20 16:14:30 -0300

    30%

--- processo-seletivo/src/Componentes/Main/index.jsx
+++ processo-seletivo/src/Componentes/Main/index.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios"
 
 export function Principal () {
     const  [grauEsfeEsq, setGrauEsfEsq] = useState('');
     const [grauEsfeDir, setGrauEsfDir] = useState('');
     const  [grauCiliEsq, setGrauCiliEsq] = useState('');
     const  [grauCiliDir, setGrauCiliDir] = useState('');
-    const res = document.getElementById('a').value
-  
     let result = document.getElementsByClassName('res')
+        
+        
 
-    
+    function btn(e) {
+        e.preventDefault()
 
-    function btn(f) {
-        f.preventDefault()
-        if (res == 1 ){
-            result.innerHTML = "uau"
-        }
     }
     
     return(
@@ -23,9 +20,9 @@ export function Principal () {
             <div className="opcoes">
                 <form id="fomr" onSubmit={btn}>
                 <p>grau esferico olho esquerdo :<input id="a" type="number" step="0.010" value={grauEsfeEsq} onChange={(e)=>setGrauEsfEsq(e.target.value)}/></p> 
-                <p>grau esferico olho direito :<input type="number" step="0.010" value={grauEsfeDir} onChange={(b)=>setGrauEsfDir(b.target.value)}/></p> 
-                <p>grau cilindrico olho esquerdo :<input type="number" step="0.010" value={grauCiliEsq} onChange={(c)=>setGrauCiliEsq(c.target.value)}/></p> 
-                <p>grau cilindrico olho direito :<input type="number" step="0.010" value={grauCiliDir} onChange={(d)=>setGrauCiliDir(d.target.value)}/> </p>
+                <p>grau esferico olho direito :<input type="number" step="0.010" value={grauEsfeDir} onChange={(e)=>setGrauEsfDir(e.target.value)}/></p> 
+                <p>grau cilindrico olho esquerdo :<input type="number" step="0.010" value={grauCiliEsq} onChange={(e)=>setGrauCiliEsq(e.target.value)}/></p> 
+                <p>grau cilindrico olho direito :<input type="number" step="0.010" value={grauCiliDir} onChange={(e)=>setGrauCiliDir(e.target.value)}/> </p>
                 <input type="submit"  value="click"/>
                 </form>
                 <div className="resut">
